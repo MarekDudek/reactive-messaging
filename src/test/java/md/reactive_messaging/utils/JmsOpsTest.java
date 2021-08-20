48fb6f79609fd261f8b8c1fd230f2a25e62fa88b
Marek Dudek <marek.dudek@gmail.com>
2021-08-20 21:14:44 +0200

    JMS operations tested

---
+++ src/test/java/md/reactive_messaging/utils/JmsOpsTest.java
@@ -0,0 +1,40 @@
+package md.reactive_messaging.utils;
+
+import com.tibco.tibjms.TibjmsConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+import md.reactive_messaging.jms.JmsOps;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+final class JmsOpsTest
+{
+    private static final String URL = "tcp://localhost:7222";
+    private static final String USER = "some-user";
+    private static final String PASSWORD = "some-password";
+    private static final String QUEUE = "demo-queue";
+
+    // System under test
+    private static final JmsOps OPS = new JmsOps();
+
+    @Test
+    void consume()
+    {
+        OPS.createConnection(new TibjmsConnectionFactory(URL), USER, PASSWORD).flatMap(newConnection ->
+                OPS.setExceptionListener(newConnection,
+                        exception -> log.error("", exception)
+                ).flatMap(listenedConnection ->
+                        OPS.createSession(newConnection).flatMap(session ->
+                                OPS.createQueue(session, QUEUE).flatMap(queue ->
+                                        OPS.createConsumer(session, queue)).flatMap(consumer ->
+                                        OPS.startConnection(listenedConnection).map(startedConnection ->
+                                                consumer
+                                        )
+                                )
+                        )
+                )
+        ).flatMap(OPS::receive).consume(
+                error -> log.error("", error),
+                message -> log.info("{}", message)
+        );
+    }
+}
