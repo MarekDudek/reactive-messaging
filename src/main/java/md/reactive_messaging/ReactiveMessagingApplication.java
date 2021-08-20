39b69601555aa973012714a9ab345752bef857fb
Marek Dudek <marek.dudek@gmail.com>
2021-08-20 19:41:17 +0200

    Fresh Spring Boot application

---
+++ src/main/java/md/reactive_messaging/ReactiveMessagingApplication.java
@@ -0,0 +1,13 @@
+package md.reactive_messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMessagingApplication.class, args);
+	}
+
+}
