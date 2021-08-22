d4211bdea5aa54261e9d70ad816349329c94f0c7
Marek Dudek <marek.dudek@gmail.com>
2021-08-22 17:22:47 +0200

    reconnect works

--- src/main/java/md/reactive_messaging/reactive/ReactiveOps.java
+++ src/main/java/md/reactive_messaging/reactive/ReactiveOps.java
@@ -1,16 +1,13 @@
 package md.reactive_messaging.reactive;
 
+import com.tibco.tibjms.TibjmsConnectionFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import md.reactive_messaging.jms.Jms2Ops;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.Sinks;
-import reactor.util.retry.Retry;
 
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSContext;
-import java.time.Duration;
 import java.util.function.Function;
 
 @Slf4j
@@ -24,6 +21,7 @@ public class ReactiveOps
         RECONNECT
     }
 
+    @Deprecated // does not use ops
     public Mono<ConnectionFactory> factoryFromCallable(Function<String, ConnectionFactory> constructor, String url)
     {
         return Mono.fromCallable(
@@ -36,6 +34,21 @@ public class ReactiveOps
         );
     }
 
+    public Mono<ConnectionFactory> factoryFromCallable2(Function<String, ConnectionFactory> constructor, String url)
+    {
+        return Mono.fromCallable(
+                () ->
+                        ops.instantiateConnectionFactory(constructor, url).apply(
+                                error -> {
+                                    log.error("Instantiating connection factory failed", error);
+                                    throw error;
+                                },
+                                factory -> factory
+                        )
+        );
+    }
+
+    @Deprecated // does not use ops
     public Mono<JMSContext> contextFromCallable(ConnectionFactory factory, String userName, String password)
     {
         return Mono.fromCallable(
@@ -48,11 +61,46 @@ public class ReactiveOps
         );
     }
 
-    public Flux<JMSContext> contexts(ConnectionFactory factory, String userName, String password, long maxAttempts, Duration minBackoff)
+    @Deprecated // does not use ops; but it logs underlying error
+    public Mono<JMSContext> contextFromCallable2(String url, String userName, String password)
     {
-        final Sinks.Many<Reconnect> reconnect = Sinks.many().unicast().onBackpressureBuffer();
-        return contextFromCallable(factory, userName, password).
-                retryWhen(Retry.backoff(maxAttempts, minBackoff)).
-                repeatWhen(repeat -> reconnect.asFlux());
+        return Mono.fromCallable(() -> {
+                    log.info("Creating factory to {}", url);
+                    final TibjmsConnectionFactory factory = new TibjmsConnectionFactory(url);
+                    log.info("Created factory {}", factory);
+                    log.info("Creating context for {} ({})", userName, password);
+                    final JMSContext context = factory.createContext(userName, password);
+                    log.info("Created context {}", context);
+                    return context;
+                }
+        );
+    }
+
+    public Mono<JMSContext> contextFromCallable3(ConnectionFactory factory, String userName, String password)
+    {
+        return Mono.fromCallable(
+                () ->
+                        ops.createContext(factory, userName, password).apply(
+                                error -> {
+                                    log.error("Creating JMS context failed", error);
+                                    throw error;
+                                },
+                                context -> context
+                        )
+        );
+    }
+
+    @Deprecated
+    public Mono<JMSContext> createContext(Function<String, ConnectionFactory> constructor, String url, String userName, String password)
+    {
+        return Mono.create(sink -> {
+                    ops.instantiateConnectionFactory(constructor, url).flatMap(factory ->
+                            ops.createContext(factory, userName, password)
+                    ).consume(
+                            sink::error,
+                            sink::success
+                    );
+                }
+        );
     }
 }
--- src/test/java/md/reactive_messaging/reactive/ReactiveOpsTest.java
+++ src/test/java/md/reactive_messaging/reactive/ReactiveOpsTest.java
@@ -7,15 +7,22 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 import reactor.core.publisher.Sinks.Many;
+import reactor.util.retry.Retry;
 
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSContext;
 import javax.jms.JMSRuntimeException;
+import java.time.Duration;
 import java.util.Optional;
 
+import static java.lang.Long.MAX_VALUE;
+import static java.lang.Thread.sleep;
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofSeconds;
 import static md.reactive_messaging.TestTibcoEmsConfig.*;
 
 @Slf4j
@@ -23,7 +30,11 @@ import static md.reactive_messaging.TestTibcoEmsConfig.*;
 final class ReactiveOpsTest
 {
     private static final Jms2Ops JOPS = new Jms2Ops();
-    private final ReactiveOps ROPS = new ReactiveOps(JOPS);
+    private static final ReactiveOps ROPS = new ReactiveOps(JOPS);
+
+    private static final long MAX_ATTEMPTS = MAX_VALUE;
+    private static final Duration MIN_BACKOFF = ofSeconds(1);
+    private static final Duration TEST_DURATION = ofSeconds(30);
 
     @Order(1)
     @Test
@@ -55,32 +66,119 @@ final class ReactiveOpsTest
 
     @Order(3)
     @Test
-    void contexts()
+    void contexts() throws InterruptedException
     {
         final Mono<ConnectionFactory> factoryM =
                 ROPS.factoryFromCallable(TibjmsConnectionFactory::new, URL);
-        final Many<Object> reconnectM = Sinks.many().unicast().onBackpressureBuffer();
+        final Many<Object> reconnectS = Sinks.many().unicast().onBackpressureBuffer();
         final Object reconnect = new Object();
         final Mono<JMSContext> contextM = factoryM.flatMap(factory ->
                 ROPS.contextFromCallable(factory, USER_NAME, PASSWORD).map(context -> {
                     final Optional<JMSRuntimeException> settingError =
-                            JOPS.setExceptionListener(context, error -> reconnectM.tryEmitNext(reconnect));
-                    settingError.ifPresent(error -> reconnectM.tryEmitNext(reconnect));
+                            JOPS.setExceptionListener(context, error -> reconnectS.tryEmitNext(reconnect));
+                    settingError.ifPresent(error -> reconnectS.tryEmitNext(reconnect));
                     return context;
                 })
         );
+        final Flux<JMSContext> contextsF = contextM.
+                retryWhen(Retry.backoff(MAX_ATTEMPTS, ofMillis(100))).
+                repeatWhen(repeat -> reconnectS.asFlux());
 
-        contextM.subscribe(
-                context -> {
-                    log.info("Context: {}", context);
-                },
-                error -> {
-                    log.error("Error: ", error);
-                },
-                () -> {
-                    log.info("Completed");
-                }
+        new Thread(() ->
+                contextsF.subscribe(
+                        context -> log.info("Context: {}", context),
+                        error -> log.error("Error: ", error),
+                        () -> log.info("Completed")
+                )
+        ).start();
+
+        sleep(TEST_DURATION.toMillis());
+    }
+
+    @Order(4)
+    @Test
+    void contexts2() throws InterruptedException
+    {
+        final Many<Object> reconnectS = Sinks.many().unicast().onBackpressureBuffer();
+        final Object reconnect = new Object();
+        final Mono<JMSContext> contextM =
+                ROPS.contextFromCallable2(URL, USER_NAME, PASSWORD).map(context -> {
+                    final Optional<JMSRuntimeException> settingError =
+                            JOPS.setExceptionListener(context, error -> reconnectS.tryEmitNext(reconnect));
+                    settingError.ifPresent(error -> reconnectS.tryEmitNext(reconnect));
+                    return context;
+                });
+        final Flux<JMSContext> contextsF = contextM.
+                retryWhen(Retry.backoff(MAX_ATTEMPTS, MIN_BACKOFF)).
+                repeatWhen(repeat -> reconnectS.asFlux());
+
+        new Thread(() ->
+                contextsF.subscribe(
+                        context -> log.info("Context: {}", context),
+                        error -> log.error("Error: ", error),
+                        () -> log.info("Completed")
+                )
+        ).start();
+
+        sleep(TEST_DURATION.toMillis());
+    }
+
+    @Order(5)
+    @Test
+    void contexts3() throws InterruptedException
+    {
+        final Many<Object> reconnectS = Sinks.many().unicast().onBackpressureBuffer();
+        final Object reconnect = new Object();
+        final Mono<JMSContext> contextM =
+                ROPS.createContext(TibjmsConnectionFactory::new, URL, USER_NAME, PASSWORD).map(context -> {
+                    final Optional<JMSRuntimeException> settingError =
+                            JOPS.setExceptionListener(context, error -> reconnectS.tryEmitNext(reconnect));
+                    settingError.ifPresent(error -> reconnectS.tryEmitNext(reconnect));
+                    return context;
+                });
+        final Flux<JMSContext> contextsF = contextM.
+                retryWhen(Retry.backoff(MAX_ATTEMPTS, MIN_BACKOFF)).
+                repeatWhen(repeat -> reconnectS.asFlux());
+
+        new Thread(() ->
+                contextsF.subscribe(
+                        context -> log.info("Context: {}", context),
+                        error -> log.error("Error: ", error),
+                        () -> log.info("Completed")
+                )
+        ).start();
+
+        sleep(TEST_DURATION.toMillis());
+    }
+
+    @Order(6)
+    @Test
+    void contexts4() throws InterruptedException
+    {
+        final Mono<ConnectionFactory> factoryM =
+                ROPS.factoryFromCallable2(TibjmsConnectionFactory::new, URL);
+        final Many<Object> reconnectS = Sinks.many().unicast().onBackpressureBuffer();
+        final Object reconnect = new Object();
+        final Mono<JMSContext> contextM = factoryM.flatMap(factory ->
+                ROPS.contextFromCallable3(factory, USER_NAME, PASSWORD).map(context -> {
+                    final Optional<JMSRuntimeException> settingError =
+                            JOPS.setExceptionListener(context, error -> reconnectS.tryEmitNext(reconnect));
+                    settingError.ifPresent(error -> reconnectS.tryEmitNext(reconnect));
+                    return context;
+                })
         );
+        final Flux<JMSContext> contextsF = contextM.
+                retryWhen(Retry.backoff(MAX_ATTEMPTS, ofMillis(100))).
+                repeatWhen(repeat -> reconnectS.asFlux());
 
+        new Thread(() ->
+                contextsF.subscribe(
+                        context -> log.info("Context: {}", context),
+                        error -> log.error("Error: ", error),
+                        () -> log.info("Completed")
+                )
+        ).start();
+
+        sleep(TEST_DURATION.toMillis());
     }
 }
