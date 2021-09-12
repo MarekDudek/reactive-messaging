75a5b7f26a414b7b079cdce1a6b09f6843fe6329
Marek Dudek <marek.dudek@gmail.com>
2021-09-12 16:43:47 +0200

    removed obsolete code

--- bin/run-tibco-sender.sh
+++ bin/run-tibco-sender.sh
@@ -2,5 +2,5 @@
 IFS=$'\n\t'
 set -euxo pipefail
 
-VERSION=0.0.3-SNAPSHOT
+VERSION=0.0.4-SNAPSHOT
 java -jar -Dspring.profiles.active=jms-sync-sender,tibco ./target/reactive-messaging-${VERSION}.jar
